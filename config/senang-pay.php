eba843c1bcddf25f39309da0d3701e2ba69b9245
php anonymous <php.anonymous1@gmail.com>
2024-12-20 17:33:21 +0200

    Update senang-pay.php

--- config/senang-pay.php
+++ config/senang-pay.php
@@ -1,21 +1,7 @@
 <?php
-/***
- *      ____            _   _       _     ____                  _    ___    __  __               
- *     | __ )   _   _  (_) | |   __| |   | __ )   _   _        | |  / _ \  |  \/  |   ___    ___ 
- *     |  _ \  | | | | | | | |  / _` |   |  _ \  | | | |    _  | | | | | | | |\/| |  / _ \  / __|
- *     | |_) | | |_| | | | | | | (_| |   | |_) | | |_| |   | |_| | | |_| | | |  | | | (_) | \__ \
- *     |____/   \__,_| |_| |_|  \__,_|   |____/   \__, |    \___/   \___/  |_|  |_|  \___/  |___/
- *                                                |___/                                          
- *
- *      @author: Idham Hafidz Bin Ismail (Apit John Kedekut Ismail)
- *      @email: idham@jomos.com.my
- *      @website: jomos.com.my
- *      @author: Mahmoud Ibrahim (php anonymous)
- *      @email:  php.anonymous1@gmail.com
- *      @website: phpdash.com
- */
-
+ 
 return [
+    'encryption_mode' => 'md5', // md5 || SHA256  
     "mode" => "sandbox", // sandbox | live
     "secret-key" => "",
     "merchant-id" => ""
